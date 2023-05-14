988bc97237b219696427e82d67b0d622cdf5dbd2
Mariam Lopez -Alumina <mlopez@grupoalumina.com.co>
2023-05-13 21:23:03 -0500

    Edit  Categories

---
+++ Data/SupermarketContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Data
+{
+    public class SupermarketContext : DbContext
+    {
+        public SupermarketContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+		internal Task SaveChangesAsyng()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
